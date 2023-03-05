da72a2608661a955f6cdcf5b3fc9c6e09dd0041f
JVS23 <juhosiren23@gmail.com>
2023-03-05 18:45:46 +0200

    sql finalized v2

--- schema.sql
+++ schema.sql
@@ -15,8 +15,8 @@ CREATE TABLE IF NOT EXISTS threads (
     title TEXT,
     content TEXT,
     likes INTEGER,
-    user_id INTEGER REFERENCES users ON DELETE CASCADE,
-    category_id INTEGER REFERENCES categories ON DELETE CASCADE,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
     created_at TIMESTAMP
 );
 
@@ -24,15 +24,15 @@ CREATE TABLE IF NOT EXISTS threads (
 CREATE TABLE IF NOT EXISTS replies (
     id SERIAL PRIMARY KEY,
     content TEXT,
-    user_id INTEGER REFERENCES users ON DELETE CASCADE,
-    thread_id INTEGER REFERENCES threads ON DELETE CASCADE,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    thread_id INTEGER REFERENCES threads(id) ON DELETE CASCADE,
     sent_at TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS likes (
     id SERIAL PRIMARY KEY,
-    liker_id INTEGER REFERENCES users ON DELETE CASCADE,
-    thread_id INTEGER REFERENCES threads ON DELETE CASCADE
+    liker_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    thread_id INTEGER REFERENCES threads(id) ON DELETE CASCADE
 );
 
 
@@ -40,3 +40,5 @@ CREATE TABLE IF NOT EXISTS likes (
 INSERT INTO categories (category_name) VALUES ('cat');
 INSERT INTO categories (category_name) VALUES ('dog');
 INSERT INTO categories (category_name) VALUES ('other');
+
+// DROP TABLE users, categories, threads, replies, likes;
